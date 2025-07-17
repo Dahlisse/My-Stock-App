ebd863a5558c980905168f21cdb2ad2d7c04fa67
Dahlisse <daollead@gmail.com>
2025-07-17 15:38:02 +0900

    Create __init__.py

---
+++ tests/performance/__init__.py
@@ -0,0 +1,8 @@
+# performance/__init__.py
+
+"""
+Performance testing package initializer.
+
+Contains modules and tests related to performance benchmarking,
+throughput measurement, and error rate monitoring of the system.
+"""
